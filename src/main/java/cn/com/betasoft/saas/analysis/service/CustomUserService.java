f87a2186c86e6ebf9cdded2537dc978dfd7037dc
lcz92416 <lcz92416@163.com>
2017-12-28 13:11:14 +0800

    修复mysql八小时清除连接&日志输出问题

--- src/main/java/cn/com/betasoft/saas/analysis/service/CustomUserService.java
+++ src/main/java/cn/com/betasoft/saas/analysis/service/CustomUserService.java
@@ -3,6 +3,7 @@ package cn.com.betasoft.saas.analysis.service;
 import cn.com.betasoft.saas.analysis.mapper.SysUserByMySqlMapper;
 import cn.com.betasoft.saas.analysis.model.SysRoleModel;
 import cn.com.betasoft.saas.analysis.model.SysUserModel;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,19 +17,27 @@ import java.util.logging.Logger;
 
 @Service
 public class CustomUserService implements UserDetailsService {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(CustomUserService.class);
+
     @Autowired
     SysUserByMySqlMapper sysuserByMySqlMapper;
 
     @Override
     public UserDetails loadUserByUsername(String userName) { //重写loadUserByUsername 方法获得 userdetails 类型用户
-        SysUserModel user = sysuserByMySqlMapper.findSysByUserName(userName);
-        if(user == null){
-            throw new UsernameNotFoundException("用户名不存在");
-        }
+        SysUserModel user = new SysUserModel();
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
-        // 用于添加用户的权限。只要把用户权限添加到authorities 就万事大吉。
-        for (SysRoleModel role : user.getRoles()) {
-            authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+
+        try {
+            user = sysuserByMySqlMapper.findSysByUserName(userName);
+            if (user == null) {
+                throw new UsernameNotFoundException("用户名不存在");
+            }
+            // 用于添加用户的权限。只要把用户权限添加到authorities 就万事大吉。
+            for (SysRoleModel role : user.getRoles()) {
+                authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+            }
+        } catch (Exception e) {
+            log.info("登录错误："+ e);
         }
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
     }
